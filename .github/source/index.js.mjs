18c804a724d0adb54645bf783948f1d814b1f8c9
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-08-10 00:03:19 +0700

    Test

--- .github/source/index.js.mjs
+++ .github/source/index.js.mjs
@@ -1,6 +1,6 @@
 import {W} from '@taufik-nurrohman/document';
 import {esc, toPattern} from '@taufik-nurrohman/pattern';
-import {fromHTML, fromStates, fromValue} from '@taufik-nurrohman/from';
+import {fromHTML, fromValue} from '@taufik-nurrohman/from';
 import {isFunction, isString} from '@taufik-nurrohman/is';
 import {that} from '@taufik-nurrohman/text-editor.source-x-m-l';
 import {toCount} from '@taufik-nurrohman/to';
@@ -12,9 +12,10 @@ const defaults = {
             a: ['a', 'link text goes here…', {}, ""],
             area: ['area', false, {}, ""],
             b: ['strong', 'text goes here…', {}, ""],
-            base: ['base', false, {}, '\n'],
+            base: ['base', false, {href: ""}, '\n'],
             blockquote: ['blockquote', "", {}, '\n'],
             br: ['br', false, {}, ["", '\n']],
+            button: ['button', 'text goes here…', {name: "", type: 'submit'}, ' '],
             code: ['code', 'code goes here…', {}, ""],
             col: ['col', false, {}, '\n'],
             em: ['em', 'text goes here…', {}, ""],
@@ -26,19 +27,23 @@ const defaults = {
             h6: ['h6', 'title goes here…', {}, '\n'],
             hr: ['hr', false, {}, '\n'],
             i: ['em', 'text goes here…', {}, ""],
-            img: ['img', false, {alt: ""}, ' '],
-            input: ['input', false, {}, ' '],
+            img: ['img', false, {alt: "", src: ""}, ' '],
+            input: ['input', false, {name: "", type: 'text'}, ' '],
             li: ['li', "", {}, '\n'],
-            link: ['link', false, {}, '\n'],
+            link: ['link', false, {href: ""}, '\n'],
             meta: ['meta', false, {}, '\n'],
             ol: ['ol', "", {}, '\n'],
             option: ['option', 'option goes here…', {}, '\n'],
             p: ['p', 'paragraph goes here…', {}, '\n'],
-            param: ['param', false, {}, '\n'],
+            param: ['param', false, {name: ""}, '\n'],
             pre: ['pre', 'text goes here…', {}, '\n'],
-            source: ['source', false, {}, '\n'],
+            script: ['script', "", {}, '\n'],
+            select: ['select', "", {name: ""}, ' '],
+            source: ['source', false, {src: ""}, '\n'],
             strong: ['strong', 'text goes here…', {}, ""],
+            style: ['style', "", {}, '\n'],
             td: ['td', "", {}, '\n'],
+            textarea: ['textarea', 'text goes here…', {name: ""}, ' '],
             th: ['th', "", {}, '\n'],
             tr: ['tr', "", {}, '\n'],
             track: ['track', false, {}, '\n'],
@@ -214,24 +219,29 @@ export function canKeyDown(key, {a, c, s}, that) {
             lineAfter = after.split('\n').shift(),
             lineBefore = before.split('\n').pop(),
             lineMatch = lineBefore.match(/^(\s+)/),
-            lineMatchIndent = lineMatch && lineMatch[1] || "", m;
+            lineMatchIndent = lineMatch && lineMatch[1] || "", m, n;
         if (!value) {
             if (after && before) {
-                let continueable = ['li', 'option', 'p', 'td', 'th'], n;
-                for (let i = 0, j = toCount(continueable); i < j; ++i) {
-                    if (!(n = object[continueable[i]])) {
-                        continue;
-                    }
-                    n = n[0];
-                    if ('</' + n + '>' === lineAfter && (m = lineBefore.match(toPattern('^\\s*' + tagStart(n), "")))) {
+                let continueOnEnterTags = ['li', 'option', 'p', 'td', 'th'];
+                for (let i = 0, j = toCount(continueOnEnterTags); i < j; ++i) {
+                    n = continueOnEnterTags[i];
+                    if (toPattern('^' + tagEnd(n), "").test(lineAfter) && (m = lineBefore.match(toPattern('^\\s*' + tagStart(n), "")))) {
                         // `<asdf>|</asdf>`
                         if (m[0] === lineBefore) {
+                            // Unwrap again if empty!
                             that.replace(toPattern(tagStart(n) + '$', ""), "", -1);
                             that.replace(toPattern('^' + tagEnd(n), ""), "", 1);
                             return that.record(), false;
                         }
                         // `<asdf>asdf|</asdf>`
                         return that.insert('</' + n + '>\n' + lineMatchIndent + '<' + n + (m[2] || "") + '>', -1).record(), false;
+                    }
+                }
+                let noIndentOnEnterTags = ['script', 'style'];
+                for (let i = 0, j = toCount(noIndentOnEnterTags); i < j; ++i) {
+                    n = noIndentOnEnterTags[i];
+                    if (toPattern('^' + tagEnd(n), "").test(lineAfter) && toPattern(tagStart(n) + '$', "").test(lineBefore)) {
+                        return that.wrap('\n' + lineMatchIndent, '\n').record(), false;
                     }
                 }
                 for (let i = 1; i < 7; ++i) {
